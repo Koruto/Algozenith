d1228b89360469551174467a602d0bab16b27b1b
Koruto <flyingshadow03@gmail.com>
2023-10-22 09:26:04 +0530

    Started Project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Hero from './components/Hero';
+import Pricing from './components/Pricing';
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <Pricing />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+function Hero() {
+  return (
+    <div>
+      {/* Add background to this div, the linear one */}
+      Working
+      {/* <AlgozenithLogo /> */}
+    </div>
+  );
+}
+
+export default Hero;
